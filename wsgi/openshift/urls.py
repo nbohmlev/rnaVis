1cb0db53551478ee92bcedc6d1eaa7386f9dccf5
Sameer Aryal <sa6@williams.edu>
2014-11-17 14:07:48 -0500

    Activating my app

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^sumStats/', include('sumStats.urls', namespace="sumStats")),
 )
